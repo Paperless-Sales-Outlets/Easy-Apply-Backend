a14324719d63133278de2fc801e11b2b2e2f2a62
Dhawanharan <dhawanmaha@gmail.com>
2026-09-08 09:56:29 +0530

    NIC-OCR for signup page

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ import productRoutes from './routes/productRoutes.js';
 import cartRoutes from './routes/cartRoutes.js';
 import customerRoutes from './routes/customerRoutes.js';
 import fileRoutes from './routes/fileRoutes.js';
+import nicRoutes from './routes/nicRoutes.js';
 
 import { errorHandler, notFound } from './middleware/errorHandler.js';
 import { requestLogger, errorLogger } from './middleware/loggingMiddleware.js';
@@ -274,6 +275,11 @@ app.use(
 app.use(
   '/api/files',
   fileRoutes
+);
+
+app.use(
+  '/api/nic',
+  nicRoutes
 );
 
 
